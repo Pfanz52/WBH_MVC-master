1130531ae712606972e1c998d9c1605a506a0872
Pfanz52 <147510551+Pfanz52@users.noreply.github.com>
2024-11-28 10:14:34 +0700

    changelocalhost

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php 
-DEFINE('INDEX_URL','http://localhost/webMau/WBH_MVC-master/');
+DEFINE('INDEX_URL','http://localhost:8022/webMau/WBH_MVC-master/');
 
 DEFINE('DB_HOST','localhost');
 DEFINE('DB_USERNAME','root');
